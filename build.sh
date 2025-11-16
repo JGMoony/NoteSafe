95fc82a480857b55d22299b32deb38f4b5f5d03d
Liseth Gomez <gomezlisj@gmail.com>
2025-11-15 19:02:47 -0500

    last?

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+python manage.py createsuperuser --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL --no-input --password $DJANGO_SUPERUSER_PASSWORD
