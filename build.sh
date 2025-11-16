64f64371905bc9e143bfff6a45e5716987fc99b5
Liseth Gomez <gomezlisj@gmail.com>
2025-11-15 19:20:39 -0500

    superuser temporal

--- build.sh
+++ build.sh
@@ -4,6 +4,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
-python manage.py migrate
-
-python manage.py createsuperuser $DJANGO_SUPERUSER_USERNAME --no-input
+python manage.py migrate && python manage.py createsuperuser --noinput && gunicorn NoteSafe.wsgi
