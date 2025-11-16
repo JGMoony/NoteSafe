0edf6da41b998ccb7256b97671328ac2db35f60d
Liseth Gomez <gomezlisj@gmail.com>
2025-11-15 20:12:47 -0500

    Add secure SocialApp fixture without secrets

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
+#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-
+python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Fix SocialApp BEFORE loading it
+sed -i "s/ENV_GOOGLE_CLIENT_ID/$GOOGLE_CLIENT_ID/g" users/fixtures/socialapp.json
+sed -i "s/ENV_GOOGLE_CLIENT_SECRET/$GOOGLE_CLIENT_SECRET/g" users/fixtures/socialapp.json
+
+python manage.py loaddata users/fixtures/socialapp.json
